8c7e8ce457ec63986e925c6bb36864c113a771a5
Vanstorm9 <antlowhur@yahoo.com>
2016-05-23 00:33:21 -0400

    Added the first three sample scripts

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # prolog-experiments
 Experimenting in the prolog language
+
+A good online Prolog interpreter to try out:
+http://pengines.swi-prolog.org/apps/swish/index.html
---
+++ tutorials/scope_struct_math/course.pl
@@ -0,0 +1,25 @@
+course(
+	cse340,
+	day(tues,thurs),
+	time(1200, 1300),
+	instructor(bryce, holton),
+	coor300
+).
+
+course(
+	cse340,
+	day(tues,thurs),
+	time(1200,1300),
+	instructor(john, johnson),
+	coor500
+).
+
+instructor(Instructor, Class) :-
+	course( Class,
+		_,
+		_,
+		Instructor,
+		_
+	).
+
+
